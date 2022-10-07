423b69f4fa8d795629920b4282d169cc657b50b3
pedrokpdo <pedromotoehm@gmail.com>
2022-10-07 16:06:06 -0300

    arrumando stack

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
+import { Space } from "../../components/funcionais/SpaceComponent";
 import { Title } from "../../components/funcionais/TitleComponent";
 
 export const ProfileScreen = () => {
-    return(
-        <Title>Profile aqui</Title>
+    return (
+        <ContainerComponent>
+            <Space>
+                <Title>Profile aqui</Title>
+            </Space>
+        </ContainerComponent>
     )
 }
--- src/stacks/bottom.stack.tsx
+++ src/stacks/bottom.stack.tsx
@@ -1,3 +1,4 @@
+import { EvilIcons, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
 import { HomeStack } from './home.stack'
@@ -7,9 +8,25 @@ const Bottom = createBottomTabNavigator()
 
 export const BottomStack = () => {
     return (
-        <Bottom.Navigator>
-            <Bottom.Screen name='HomeStack' component={HomeStack} options={{ headerShown: false }} />
-            <Bottom.Screen name='ProfileStack' component={ProfileStack} options={{ headerShown: false }} />
+        <Bottom.Navigator
+            screenOptions={{ headerShown: false, tabBarLabelStyle: { fontSize: 16 }, tabBarInactiveTintColor: 'gray', tabBarActiveTintColor: 'purple' }}
+        >
+            <Bottom.Screen
+                name='HomeStack'
+                component={HomeStack}
+                options={{
+                    tabBarIcon: ({ color }) => (<MaterialCommunityIcons name='home' size={24} color={color} />),
+                    tabBarLabel: 'Home'
+                }}
+            />
+            <Bottom.Screen
+                name='ProfileStack'
+                component={ProfileStack}
+                options={{
+                    tabBarIcon: ({ color }) => (<MaterialCommunityIcons name='face-man-profile' size={24} color={color} />),
+                    tabBarLabel: 'Perfil'
+                }}
+            />
         </Bottom.Navigator>
     )
 }
