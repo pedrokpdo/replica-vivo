965773e626ca754d47f3ac62a1d3a0cc48c60563
pedrokpdo <pedromotoehm@gmail.com>
2022-10-07 18:12:54 -0300

    icons header

--- src/screens/HeaderComponent/index.tsx
+++ src/screens/HeaderComponent/index.tsx
@@ -1,11 +1,19 @@
+import { EvilIcons, MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import { View } from "react-native";
+import { Row } from "../../components/funcionais/RowComponent";
 
 
 export const HeaderComponent = ({ children }) => {
     return (
-        <View style={{ backgroundColor: 'purple', paddingVertical: 60 }}>
-            {children}
+        <View style={{ backgroundColor: 'purple' }}>
+            <Row top={2} style={{ alignItems: 'center', justifyContent: 'space-around', width: 80, alignSelf: 'flex-end', marginRight: 8 }}>
+                <MaterialIcons name="refresh" size={24} color='white' />
+                <MaterialIcons name="person-add" size={24} color='white' />
+            </Row>
+            <View style={{ paddingVertical: 60 }}>
+                {children}
+            </View>
         </View>
     )
 }
