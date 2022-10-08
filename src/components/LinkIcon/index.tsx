aa5573f05a958f3a5c709308a416fae4ffb04f01
pedrokpdo <pedromotoehm@gmail.com>
2022-10-07 22:30:22 -0300

    LinkIconComponent

---
+++ src/components/LinkIcon/index.tsx
@@ -0,0 +1,13 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { View } from "react-native";
+import { Title } from "../funcionais/TitleComponent";
+
+export const LinkIcon = ({ name, iconName, Icon }) => {
+    return (
+        <View style={{ backgroundColor: 'white', width: 180, height: 100, padding: 16, borderRadius: 8 }}>
+            <Icon name={iconName} color={'purple'} size={24} />
+            <Title top={2}>{name}</Title>
+        </View>
+    )
+}
--- src/screens/DiscoveryScreen/index.tsx
+++ src/screens/DiscoveryScreen/index.tsx
@@ -1,10 +1,23 @@
+import { Feather, Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
+import { Row } from "../../components/funcionais/RowComponent";
+import { Space } from "../../components/funcionais/SpaceComponent";
+import { Title } from "../../components/funcionais/TitleComponent";
+import { LinkIcon } from "../../components/LinkIcon";
+import { HeaderComponent } from "../HeaderComponent";
 
 export const DiscoveryScreen = () => {
     return (
         <ContainerComponent>
-            
+            <HeaderComponent>
+                <Space>
+                    <Title style={{ color: 'white', fontSize: 30 }}>Descubra</Title>
+                    <Row>
+                        <LinkIcon iconName='shopping-bag' name='Shopping' Icon={Feather} />
+                    </Row>
+                </Space>
+            </HeaderComponent>
         </ContainerComponent>
     )
 }
