393962497f0799fcd4e1377a031d1fd4b1deade7
pedrokpdo <pedromotoehm@gmail.com>
2022-10-07 23:40:05 -0300

    Suporte Tecnico

--- src/screens/SuportScreen/index.tsx
+++ src/screens/SuportScreen/index.tsx
@@ -1,14 +1,32 @@
 import React from "react";
+import { Image, View } from "react-native";
+import { CardComponent } from "../../components/CardComponent";
 import { ContainerComponent } from "../../components/funcionais/ContainerComponent";
 import { Space } from "../../components/funcionais/SpaceComponent";
+import { Text } from "../../components/funcionais/TextComponent";
 import { Title } from "../../components/funcionais/TitleComponent";
+import { LinkComponent } from "../../components/LinkComponent";
+import { HeaderComponent } from "../HeaderComponent";
+//@ts-ignore
+import cartoon from '../../assets/cartoon.png'
 
 export const SuportScreen = () => {
     return (
         <ContainerComponent>
-            <Space>
-                <Title>Esta é a ₢₢₢₢</Title>
-            </Space>
+            <HeaderComponent>
+                <Space>
+                    <Title top={-4} style={{ color: 'white', fontSize: 30 }}>Suporte</Title>
+                    <Text top={1} bottom={4} style={{ color: 'white', fontSize: 26 }}>Como podemos ajudar?</Text>
+                    <CardComponent style={{ backgroundColor: 'white', padding: 16, flexDirection: 'row', justifyContent: 'space-between', marginBottom: -150, borderColor: 'white' }}>
+                        <View style={{ width: 250 }}>
+                            <Title bold>Suporte tecnico</Title>
+                            <Text bottom={4}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Text>
+                            <LinkComponent name="Preciso de ajuda" />
+                        </View>
+                        <Image source={cartoon} style={{ width: 70, height: 150, marginBottom: -16 }}/>
+                    </CardComponent>
+                </Space>
+            </HeaderComponent>
         </ContainerComponent>
     )
 }
