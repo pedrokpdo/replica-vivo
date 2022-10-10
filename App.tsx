8886b06453ae89eeed44b4c184a7bb16d47e01cc
pedrokpdo <pedromotoehm@gmail.com>
2022-10-09 23:01:29 -0300

    ajustes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { AppStack } from './src/AppStack';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppStack/>
+      <AppStack />
+      <StatusBar translucent={false} backgroundColor={'purple'} />
     </NavigationContainer>
   );
 }
--- src/components/funcionais/ContainerComponent/index.tsx
+++ src/components/funcionais/ContainerComponent/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 
 export const ContainerComponent = ({ children }) => {
     return (
-        <View style={{ paddingTop: 30, flex: 1, backgroundColor: '#e4e4e4' }}>
+        <ScrollView style={{ flex: 1, backgroundColor: '#e4e4e4' }}>
             {children}
-        </View>
+            <View style={{ height: 16 }}/>
+        </ScrollView>
     )
 }
