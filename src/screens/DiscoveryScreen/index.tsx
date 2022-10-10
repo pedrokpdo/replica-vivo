58f90bf97ce93ff1ad804fa65c478b60858f126a
pedrokpdo <pedromotoehm@gmail.com>
2022-10-10 14:47:32 -0300

    recharge in stack

--- src/screens/DiscoveryScreen/index.tsx
+++ src/screens/DiscoveryScreen/index.tsx
@@ -22,7 +22,7 @@ export const DiscoveryScreen = () => {
                             <LinkIcon onPress={() => { navigation.navigate('ShoppingScreen') }} iconName='shopping-bag' name='Shopping' Icon={Feather} />
                         </View>
                         <View style={{ marginRight: 16 }}>
-                            <LinkIcon iconName='shopping-bag' name='Shopping' Icon={Feather} />
+                            <LinkIcon onPress={() => { navigation.navigate('RechargeScreen') }} iconName='shopping-bag' name='Shopping' Icon={Feather} />
                         </View>
                         <View style={{ marginRight: 16 }}>
                             <LinkIcon iconName='shopping-bag' name='Shopping' Icon={Feather} />
--- src/stacks/DiscoveryStack/index.tsx
+++ src/stacks/DiscoveryStack/index.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { DiscoveryScreen } from "../../screens/DiscoveryScreen";
+import { RechargeScreen } from "../../screens/RechargeScreen";
 import { ShoppingScreen } from "../../screens/ShoppingScreen";
 
 
@@ -11,6 +12,7 @@ export const DiscoveryStack = () => {
         <Navigator>
             <Screen name="DiscoveryScreen" component={DiscoveryScreen} options={{ headerShown: false }} />
             <Screen name="ShoppingScreen" component={ShoppingScreen} options={{ headerShown: false }} />
+            <Screen name="RechargeScreen" component={RechargeScreen} options={{ headerShown: false }} />
         </Navigator>
     )
 }
